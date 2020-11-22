e076654655664809427b35da2cea0222673489e3
Sandro Andrade <sandro.andrade@nitryx.com>
2020-11-21 22:42:27 -0300

    Estudo dos Microserviços usando Spring Cloud e Zuul como Proxy

---
+++ convert-client/src/test/java/com/schoolofnet/convertclient/ConvertClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolofnet.convertclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ kelvin-client/src/test/java/com/schoolofnet/kelvinclient/KelvinClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolofnet.kelvinclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KelvinClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
